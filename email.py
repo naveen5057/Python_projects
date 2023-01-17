68d76a72eb701feb8c81ffecae4053eefbb31988
Dheeraj05122001 <118016465+Dheeraj05122001@users.noreply.github.com>
2023-01-17 12:13:46 +0530

    Update email.py

--- email.py
+++ email.py
@@ -1,12 +1,17 @@
 import re
 #email validation
 a = 1
-while a==1:
-    txt = input("enter mail id: ")
-    pattern = "[A-Z]+[a-z]+\d+[@!#$%^&*_]+[@][a-z]{5,7}[.][a-z]{2,3}"
-    x = re.search(pattern,txt)
-    if re.search(pattern,txt):
-        print("valid email-id")
-        print(x.string)
-    else:
-        print("invalid email-id")
+pattern = "[A-Z]+[a-z]+\d+[@!#$%^&*_]+[@][a-z]{5,7}[.][a-z]{2,3}"
+def email():
+    while a==1:
+        txt = input("enter mail id: ")
+        x = re.search(pattern,txt)
+        if re.search(pattern,txt):
+            print("valid email-id")
+            print(x.string)
+            return True
+        else:
+            print("invalid email-id")
+            return False
+y = email()
+print(y)
